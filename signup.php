5aa321107680a32f5cfd52a765f36ae6a9bf7b06
Abenezer Amare <abenezerame@gmail.com>
2024-03-11 15:50:19 +0300

    Feat: signup

---
+++ signup.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    include_once "config.php";
+
+    // Get username, email, and password from the form
+    $firstname = $_POST["frist_name"];
+    $lastname = $_POST["last_name"];
+    $email = $_POST["email"];
+    $phonenumber = $_POST["phone_number"];
+    $password = $_POST["password"];
+    $passwordconfirm = $_POST["password-confirm"];
+
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Prepare SQL statement
+    $stmt = $conn->prepare("INSERT INTO patients (first_name, last_name, email, password, phone_number) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $firstname, $lastname, $email, $hashed_password, $phonenumber);
+
+    // Execute the statement
+    if ($stmt->execute()) {
+        $_SESSION["email"] = $email;
+        header("Location: blog.php");
+        exit();
+    } else {
+        echo "Error registering user: " . $conn->error;
+    }
+}
+?>
